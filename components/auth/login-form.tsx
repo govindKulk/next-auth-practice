9402e41c00ea9de9441edd8bd9399223da54eb84
govindKulk <kulkarnigovind2003@gmail.com>
2024-08-18 01:49:59 +0530

    implemented auth login layout

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import { useState, useTransition } from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
+import CardWrapper from '@/components/auth/card-wrapper';
+import { LoginSchema, TLoginSchema } from '@/schemas';
+import { Input } from '@/components/ui/input';
+
+import { Button } from '@/components/ui/button';
+import FormError from '@/components/form-error';
+import FormSuccess from '@/components/form-success';
+import { login } from '@/actions/login';
+
+function LoginForm() {
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
+
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<TLoginSchema>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleSubmit = (values: TLoginSchema) => {
+    startTransition(() => {
+      login(values)
+        .then((data) => {
+          setError(data?.error);
+          setSuccess(data?.success);
+        })
+        .catch(() =>
+          setError(
+            'Something happenend while tyring to log you in! Please try again.'
+          )
+        );
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel='Welcome Back'
+      backButtonLabel="Don't have an account?"
+      backButtonHref='/auth/register'
+      showSocial>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-4'>
+          <div className='space-y-4'>
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder='john.doe@example.com'
+                      type='email'
+                      required
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder='******'
+                      type='password'
+                      required
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+
+          <Button type='submit' disabled={isPending} className='w-full'>
+            Login
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+}
+export default LoginForm;

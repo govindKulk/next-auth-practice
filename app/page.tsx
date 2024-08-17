6a2ef206f1ad3bbd7c50d7a95fc7e46ea64de95d
govindKulk <kulkarnigovind2003@gmail.com>
2024-08-17 18:56:08 +0530

    initial setup and code

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (  
+    <div className="">
+      <Button variant="default" size="lg"> Click Me </Button>
+    </div>
+  );
+}

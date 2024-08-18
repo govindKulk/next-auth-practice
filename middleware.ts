dfb9c95f8186820799b21f3f1f552b7b001ad393
govindKulk <kulkarnigovind2003@gmail.com>
2024-08-19 02:12:09 +0530

    modified middleware to implement protected , public and api routes

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,51 @@
-import authConfig from "@/auth.config"
-import NextAuth from "next-auth";
+import NextAuth from 'next-auth';
+import authConfig from '@/auth.config';
 
-const {auth} = NextAuth(authConfig)
- 
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  authRoutes,
+  apiAuthPrefix,
+  publicRoutes,
+} from '@/routes';
+import { NextRequest } from 'next/server';
+
+const { auth } = NextAuth(authConfig);
+
 export default auth((req) => {
-    const isLoggedIn = !!req.auth;
-    console.log(req.nextUrl.pathname)
-    console.log('isLoggedIn', isLoggedIn)
-})
- 
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isAPiAuthPrefix = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isAPiAuthPrefix) return;
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+    return Response.redirect(
+      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+    );
+  }
+
+  return;
+});
+
 // Optionally, don't invoke Middleware on some paths
+// Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-  matcher: [    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)'],
-}
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};

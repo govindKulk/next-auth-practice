402f3bab06d2b33689b537db88f10109c852a3aa
govindKulk <kulkarnigovind2003@gmail.com>
2024-08-19 02:43:06 +0530

    added logout functionality

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,15 +1,27 @@
+
 import React from 'react'
-import {auth} from '@/auth'
+import { auth, signOut } from '@/auth'
+import { Button } from '@/components/ui/button';
 
 const SettingsPage = async () => {
 
     const session = await auth();
 
+
     return (
-    <div>
-      {JSON.stringify(session)}
-    </div>
-  )
+        <div>
+            {JSON.stringify(session)}
+
+            <form action = {async () => {
+                'use server'
+                await signOut( )
+            }}>
+                <Button size={'lg'} variant={'default'}>
+                    Logout
+                </Button>
+            </form>
+        </div>
+    )
 }
 
 export default SettingsPage

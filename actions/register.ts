dc5cd11b438aa30d10bf30317ccc6c0c6539110a
govindKulk <kulkarnigovind2003@gmail.com>
2024-08-19 00:25:28 +0530

    modified register actions to work with the database

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 'use server';
 
+import { getUserByEmail } from '@/data/user';
 import { RegisterSchema, TRegisterSchema } from '@/schemas';
+import { db } from '@/lib/db';
+import bcrypt from 'bcrypt';
 
 export const register = async (values: TRegisterSchema) => {
   console.table(values);
@@ -9,6 +12,24 @@ export const register = async (values: TRegisterSchema) => {
   if (!validatedFields.success) {
     return { error: 'Invalid field values!' };
   }
+
+  const {email, name, password} = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: 'Email already exists!' };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Create user
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password : hashedPassword
+    }
+  }) 
 
   return { success: 'Login details sent' };
 };

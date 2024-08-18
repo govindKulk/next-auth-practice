e3b28dcb789a2b5c5db31d3e83d9220beeb5174d
govindKulk <kulkarnigovind2003@gmail.com>
2024-08-19 02:40:06 +0530

    exported signin and signout from auth

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import authConfig from "@/auth.config"
 import { db } from "./lib/db"
 import { PrismaAdapter } from "@auth/prisma-adapter"
  
-export const { auth, handlers : {GET, POST} } = NextAuth({
+export const { auth, handlers : {GET, POST}, signIn, signOut } = NextAuth({
   ...authConfig,
   adapter: PrismaAdapter(db),
   session: {

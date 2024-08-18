ec90dc17af711d70d395d2277dc91d45d31f1565
govindKulk <kulkarnigovind2003@gmail.com>
2024-08-19 02:42:46 +0530

    implemented login action using credentials provider:

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,9 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { LoginSchema, TLoginSchema } from '@/schemas';
+import { AuthError } from 'next-auth';
 
 export const login = async (values: TLoginSchema) => {
   console.table(values);
@@ -10,5 +13,23 @@ export const login = async (values: TLoginSchema) => {
     return { error: 'Invalid field values!' };
   }
 
-  return { success: 'Login details sent' };
+  const {email, password} = validatedFields.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT
+    })
+  }catch(error) {
+    if(error instanceof AuthError){
+
+      switch(error.type){
+        case 'CredentialsSignin': return { error: 'Invalid credentials!' };
+        default: return { error: 'An error occurred!' };
+      }
+    }
+
+    throw error;
+  }
 };
